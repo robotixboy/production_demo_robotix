fa93ab8cbd5eb228c7e0f715dcc92fc7380f3a44
Samuel <Samuelcross2001@gmail.com>
2023-09-28 13:51:55 -0400

    Complete Database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_28_162953) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_28_175040) do
+  create_table "answers", force: :cascade do |t|
+    t.string "response"
+    t.integer "student_id"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["student_id"], name: "index_answers_on_student_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "QuestionText"
+    t.string "QuestionAnswer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "teachers", force: :cascade do |t|
     t.string "username"
     t.string "password"
@@ -18,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_162953) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "students"
 end
